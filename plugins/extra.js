ea7f3215cab9f23baa9ea9ab8d90f07e8d309630
kavishkaya <91202334+kavishkaya@users.noreply.github.com>
2021-10-03 12:51:45 +0530

    Update extra.js

--- plugins/extra.js
+++ plugins/extra.js
@@ -130,7 +130,7 @@ if (config.WORKTYPE == 'private') {
 
     MyPnky.addCommand({pattern: 'bot ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
@@ -292,7 +292,7 @@ else if (config.WORKTYPE == 'public') {
 
     MyPnky.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+	const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
