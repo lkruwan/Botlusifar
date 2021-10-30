0ee74bbcf7bf3af101985b665122e47c0f5d6d4a
kavishkaya <91202334+kavishkaya@users.noreply.github.com>
2021-10-30 10:15:44 +0530

    Update xxx.js

--- plugins/xxx.js
+++ plugins/xxx.js
@@ -1,4 +1,8 @@
 
+
+//Api එක උස්සන එකාගෙ මුලු පව්ලම හෙන හතක් වැදිලා 
+//මකබෑවිලාම යන්න ඕනෙ..!!!! eeeew!
+
 const lusifar = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
@@ -11,10 +15,12 @@ const NO_RESULT = "*🌀can't Find Anything...*"
 
 
 //උස්සන්නද ආවෙ බේසිකයෝ.බොහොම අමාරුවෙන් හැදුවෙ.උස්සන එකා අවජාතකයෙක් කියල හිතාගන්න පුලුවන් පොන්න හැත්ත.හුකන පොන්නයෝ
-    lusifar.addCommand({ pattern: 'xyz ?(.*)', fromMe: false, deleteCommand: false, desc:'video download',  deleteCommand: false}, async (message, match) => {
+    
+    lusifar.addCommand({ pattern: 'video ?(.*)', fromMe: false, deleteCommand: false, desc:'video download',  deleteCommand: false}, async (message, match) => {
         const linkk = match[1]
         if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
-        
+            await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text);
+
         await axios
           .get(`https://hardianto-chan.herokuapp.com/api/download/play?query=${linkk}&apikey=hardianto`)
           .then(async (response) => {
@@ -31,3 +37,65 @@ const NO_RESULT = "*🌀can't Find Anything...*"
        
       },
     )
+
+
+    lusifar.addCommand({ pattern: 'video ?(.*)', fromMe: false, deleteCommand: false, desc: YTV_DESC ,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        await axios
+          .get(`https://hardianto-chan.herokuapp.com/api/download/ytdownload?url=${linkk}&apikey=hardianto`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text);
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+
+//උස්සන්නද ආවෙ බේසිකයෝ.බොහොම අමාරුවෙන් හැදුවෙ.උස්සන එකා අවජාතකයෙක් කියල හිතාගන්න පුලුවන් පොන්න හැත්ත.හුකන පොන්නයෝ
+    
+    lusifar.addCommand({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc:'video download',  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+            await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text);
+
+        await axios
+          .get(`https://hardianto-chan.herokuapp.com/api/download/play?query=${linkk}&apikey=hardianto`)
+          .then(async (response) => {
+            const {
+              url,title,thumbnail,ago,views
+            } = response.data.information
+            const videoBuffer = await axios.get(thumbnail, {responseType: 'arraybuffer'})
+            
+            const cptt = "*📑Title:*  " + title + "\n\n" + "*📅Uploded date:* " + ago + "\n\n" + "*🎞️ Video link:* " +  url + "\n\n" + "*👁️Views:* " + views
+            
+
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.image, {quoted: message.data ,mimetype: Mimetype.jpg, ptt: false,caption: cptt})
+        })
+       
+      },
+    )
+
+
+    lusifar.addCommand({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc: YTV_DESC ,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        await axios
+          .get(`https://hardianto-chan.herokuapp.com/api/download/ytdownload?url=${linkk}&apikey=hardianto`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text);
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )    
